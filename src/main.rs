14b057c6dfc7108560646df5a754d94f4d331e57
hooneun <kimhoon0316@gmail.com>
2022-01-30 00:09:10 +0900

    JWT Token Auth

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
+use std::fmt::Display;
 use std::net::SocketAddr;
-use axum::{AddExtensionLayer, Router, response::Json};
-use axum::extract::Extension;
-use axum::routing::get;
-use sqlx::{postgres::PgPoolOptions, MySqlPool, Pool, MySql, PgPool};
-use serde_json::{Value, json};
-use serde::{Serialize, Deserialize};
+
+use axum::{
+    AddExtensionLayer,
+    async_trait, extract::{Extension, FromRequest, RequestParts, TypedHeader},
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::{get, post},
+    Router,
+    Json,
+};
+use chrono::{Duration, Utc};
+use headers::{Authorization, authorization::Bearer};
+use jsonwebtoken::{decode, DecodingKey, encode, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use sqlx::{MySql, MySqlPool, PgPool, Pool};
 use tower::ServiceBuilder;
 use tower_http::trace::TraceLayer;
 
@@ -28,6 +39,8 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(index))
+        .route("/authorize", post(authorize))
+        .route("/protected", get(protected))
         .layer(middleware);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -38,18 +51,21 @@ async fn main() {
         .unwrap();
 }
 
-async fn index(
-    Extension(pool): Extension<Pool<MySql>>
-) -> Json<Value> {
+async fn protected(claims: Claims) -> Result<String, AuthError> {
+    Ok(format!(
+        "Welcome to the protected area :)\nYour data:\n{}",
+        claims
+    ))
+}
+
+async fn index(Extension(pool): Extension<Pool<MySql>>) -> Json<Value> {
     let user = sqlx::query_as::<_, User>("SELECT id, name FROM users WHERE login_id = ?")
         .bind("solomon2")
         .fetch_one(&pool)
         .await;
 
     match user {
-        Ok(user) => {
-            Json(json!(user))
-        },
+        Ok(user) => Json(json!(user)),
         Err(_) => Json(json!("")),
     }
 }
@@ -67,13 +83,6 @@ struct LoginInput {
     password: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Claims {
-    sub: String,
-    company: String,
-    exp: usize,
-}
-
 #[derive(Debug, Deserialize)]
 struct AuthPayload {
     user_id: String,
@@ -84,6 +93,15 @@ struct AuthPayload {
 struct AuthBody {
     access_token: String,
     token_type: String,
+}
+
+impl AuthBody {
+    fn new(access_token: String) -> Self {
+        Self {
+            access_token,
+            token_type: "Bearer".to_string(),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -98,4 +116,82 @@ enum AuthError {
 struct User {
     id: u64,
     name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: u64,
+    exp: i64,
+    iat: i64,
+}
+
+impl Display for Claims {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "UserId: {}", self.sub)
+    }
+}
+
+impl Claims {
+    pub fn new(id: u64) -> Self {
+        let iat = Utc::now();
+        let exp = iat + Duration::hours(24);
+
+        Self {
+            sub: id,
+            iat: iat.timestamp(),
+            exp: exp.timestamp(),
+        }
+    }
+}
+
+async fn authorize(Json(payload): Json<AuthPayload>) -> Result<Json<AuthBody>, AuthError> {
+    if payload.user_id.is_empty() || payload.password.is_empty() {
+        return Err(AuthError::MissingCredentials);
+    }
+
+    let id: u64 = 444;
+    let claims = Claims::new(id);
+
+    let token = encode(&Header::default(), &claims, &EncodingKey::from_secret("secret".as_bytes()))
+        .map_err(|_| AuthError::TokenCreation)?;
+
+
+    Ok(Json(AuthBody::new(token)))
+}
+
+#[async_trait]
+impl<B> FromRequest<B> for Claims
+    where
+        B: Send,
+{
+    type Rejection = AuthError;
+
+    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
+        let TypedHeader(Authorization(bearer)) =
+            TypedHeader::<Authorization<Bearer>>::from_request(req)
+                .await
+                .map_err(|_| AuthError::InvalidToken)?;
+
+        let token_data = decode::<Claims>(bearer.token(), &DecodingKey::from_secret("secret".as_bytes()), &Validation::default())
+            .map_err(|_| AuthError::InvalidToken)?;
+
+        Ok(token_data.claims)
+    }
+}
+
+impl IntoResponse for AuthError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            AuthError::WrongCredentials => (StatusCode::UNAUTHORIZED, "Wrong credentials"),
+            AuthError::MissingCredentials => (StatusCode::BAD_REQUEST, "Missing credentials"),
+            AuthError::TokenCreation => (StatusCode::INTERNAL_SERVER_ERROR, "Token creation error"),
+            AuthError::InvalidToken => (StatusCode::BAD_REQUEST, "Invalid token"),
+        };
+
+        let body = Json(json!({
+            "error": error_message,
+        }));
+
+        (status, body).into_response()
+    }
 }
